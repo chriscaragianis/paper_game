ad3a5d420e124a49eccace2a50d9cee84128e45a
Chris Caragianis <chris.caragianis@gmail.com>
2015-03-10 22:16:43 -0400

    Added project as is to repo.  As it stands we have a random walk on the map as it grows.  What the hell is this text highlighting? I don't... And now it's regular again? So much I don't understand.  Anyhow, we are also writing a .dot file that can be fed to graphviz.  Onward.

---
+++ Room.java
@@ -0,0 +1,50 @@
+public class Room {
+
+ private int[] nbrs;
+ 
+ public Room(int parent, int newLoc)
+ {
+  nbrs = new int[4];
+  for (int i = 0; i < 4; i++)
+   nbrs[i] = -1;
+  nbrs[0] = parent;
+ }
+ 
+ public Room()
+ {
+   nbrs = new int[4];
+   for (int i = 0; i < 4; i++)
+     nbrs[i] = -1;
+ }
+ 
+ public int[] getNbrs()
+ {
+   return nbrs;
+ }
+ 
+ public boolean isOpen()
+ {
+  for (int v : nbrs)
+   if (v == -1)
+    return true;
+  return false;
+ }
+ 
+ public boolean isNbr(int index)
+ {
+   for (int v: nbrs)
+     if (v == index)
+        return true;
+   return false;
+ }
+ public void connect(int parent)
+ {
+  for (int i = 0; i < 4; i++)
+   if (nbrs[i] == -1)
+   { 
+    nbrs[i] = parent;
+    break;
+   }
+ }
+ 
+}
