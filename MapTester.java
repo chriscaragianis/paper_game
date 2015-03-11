f16c89ae9341ff9ed95bb6449272c3b10de7f502
chris caragianis <chris.caragianis@gmail.com>
2015-03-11 09:50:03 -0400

    More indention fixing 2
    Because I had my first Git screwup.  Which seems kind of difficult to do

--- MapTester.java
+++ MapTester.java
@@ -1,14 +1,14 @@
 public class MapTester
 {
-  public static void main(String[] argv)
-  {
-    Map mappy = new Map();
-    for (int i = 0; i < 10000; i++)
+    public static void main(String[] argv)
     {
-      mappy.update();
-    }
-    mappy.printMap();
-    mappy.dotMap();
-  }
+        Map mappy = new Map();
+        for (int i = 0; i < 10000; i++)
+        {
+           mappy.update();
+        }
+        mappy.printMap();
+        mappy.dotMap();
+    } 
   
 }
